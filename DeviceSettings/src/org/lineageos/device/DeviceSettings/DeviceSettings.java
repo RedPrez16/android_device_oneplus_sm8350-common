2a322b9ce4499305fdd8f442fe1f77953c9f2cce
Red <iredgaming16@gmail.com>
2021-10-16 17:52:43 +0000

    y

--- DeviceSettings/src/org/lineageos/device/DeviceSettings/DeviceSettings.java
+++ DeviceSettings/src/org/lineageos/device/DeviceSettings/DeviceSettings.java
@@ -1,68 +1,50 @@
 /*
-* Copyright (C) 2016 The OmniROM Project
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-*/
+ * Copyright (C) 2018-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.lineageos.device.DeviceSettings;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.ComponentName;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.text.TextUtils;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
 import android.util.Log;
-import androidx.preference.PreferenceFragment;
-<<<<<<< HEAD
-import androidx.preference.PreferenceManager;
-import androidx.preference.Preference;
-=======
->>>>>>> parent of 5c570cd... sm8350-common: DeviceSettings: Import Awesome Alert Slider
-import androidx.preference.SwitchPreference;
+import android.view.MenuItem;
 import androidx.preference.ListPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceManager;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.TwoStatePreference;
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+
+import java.util.Arrays;
+
+import org.lineageos.device.DeviceSettings.Constants;
+import org.lineageos.internal.util.FileUtils;
 
 public class DeviceSettings extends PreferenceFragment
         implements Preference.OnPreferenceChangeListener {
-<<<<<<< HEAD
     private static final String TAG = DeviceSettings.class.getSimpleName();
 
     private static final String KEY_USB2_SWITCH = "usb2_fast_charge";
     private static final String KEY_VIBSTRENGTH = "vib_strength";
-=======
-    public static final String KEY_SETTINGS_PREFIX = "device_setting_";
-    public static final String KEY_VIBSTRENGTH = "vib_strength";
->>>>>>> parent of 5c570cd... sm8350-common: DeviceSettings: Import Awesome Alert Slider
 
     private static final String FILE_FAST_CHARGE = "/sys/module/oplus_chg/parameters/force_fast_charge";
     private static final String FILE_LEVEL = "/sys/devices/platform/soc/88c000.i2c/i2c-10/10-005a/leds/vibrator/level";
@@ -84,16 +66,6 @@ public class DeviceSettings extends PreferenceFragment
         addPreferencesFromResource(R.xml.main);
         getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mTopKeyPref = (ListPreference) findPreference(Constants.NOTIF_SLIDER_TOP_KEY);
-        mTopKeyPref.setValueIndex(Constants.getPreferenceInt(getContext(), Constants.NOTIF_SLIDER_TOP_KEY));
-        mTopKeyPref.setOnPreferenceChangeListener(this);
-        mMiddleKeyPref = (ListPreference) findPreference(Constants.NOTIF_SLIDER_MIDDLE_KEY);
-        mMiddleKeyPref.setValueIndex(Constants.getPreferenceInt(getContext(), Constants.NOTIF_SLIDER_MIDDLE_KEY));
-        mMiddleKeyPref.setOnPreferenceChangeListener(this);
-        mBottomKeyPref = (ListPreference) findPreference(Constants.NOTIF_SLIDER_BOTTOM_KEY);
-        mBottomKeyPref.setValueIndex(Constants.getPreferenceInt(getContext(), Constants.NOTIF_SLIDER_BOTTOM_KEY));
-        mBottomKeyPref.setOnPreferenceChangeListener(this);
-
         mVibrator = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
 
@@ -108,7 +80,6 @@ public class DeviceSettings extends PreferenceFragment
         }
 
         mVibratorStrengthPreference =  (CustomSeekBarPreference) findPreference(KEY_VIBSTRENGTH);
-<<<<<<< HEAD
         if (Utils.fileWritable(FILE_LEVEL)) {
             mVibratorStrengthPreference.setValue(sharedPrefs.getInt(KEY_VIBSTRENGTH,
                 Integer.parseInt(Utils.getFileValue(FILE_LEVEL, DEFAULT))));
@@ -134,15 +105,10 @@ public class DeviceSettings extends PreferenceFragment
     private void registerPreferenceListener(String key) {
         Preference p = findPreference(key);
         p.setOnPreferenceChangeListener(this);
-=======
-        mVibratorStrengthPreference.setValue(Integer.parseInt(Utils.getFileValue(FILE_LEVEL, DEFAULT)));
-        mVibratorStrengthPreference.setOnPreferenceChangeListener(this);
->>>>>>> parent of 5c570cd... sm8350-common: DeviceSettings: Import Awesome Alert Slider
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-<<<<<<< HEAD
         if (preference == mUSB2FastChargeModeSwitch) {
             boolean enabled = (Boolean) newValue;
             SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
@@ -154,28 +120,42 @@ public class DeviceSettings extends PreferenceFragment
             SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
             sharedPrefs.edit().putInt(KEY_VIBSTRENGTH, value).commit();
             Utils.writeValue(FILE_LEVEL, String.valueOf(value));
-=======
-        if (preference == mTopKeyPref) {
-            Constants.setPreferenceInt(getContext(), preference.getKey(), Integer.parseInt((String) newValue));
-            return true;
-        } else if (preference == mMiddleKeyPref) {
-            Constants.setPreferenceInt(getContext(), preference.getKey(), Integer.parseInt((String) newValue));
-            return true;
-        } else if (preference == mBottomKeyPref) {
-            Constants.setPreferenceInt(getContext(), preference.getKey(), Integer.parseInt((String) newValue));
-            return true;
-        } else if (preference == mVibratorStrengthPreference) {
-    	    Utils.writeValue(FILE_LEVEL, String.valueOf(newValue));
-            Settings.System.putString(getContext().getContentResolver(), SETTINGS_KEY, String.valueOf(newValue));
->>>>>>> parent of 5c570cd... sm8350-common: DeviceSettings: Import Awesome Alert Slider
             mVibrator.vibrate(testVibrationPattern, -1);
             return true;
         }
+
+        String key = preference.getKey();
+        switch (key) {
+            case Constants.NOTIF_SLIDER_USAGE_KEY:
+                return handleSliderUsageChange((String) newValue) &&
+                        handleSliderUsageDefaultsChange((String) newValue) &&
+                        notifySliderUsageChange((String) newValue);
+            case Constants.NOTIF_SLIDER_ACTION_TOP_KEY:
+                return notifySliderActionChange(0, (String) newValue);
+            case Constants.NOTIF_SLIDER_ACTION_MIDDLE_KEY:
+                return notifySliderActionChange(1, (String) newValue);
+            case Constants.NOTIF_SLIDER_ACTION_BOTTOM_KEY:
+                return notifySliderActionChange(2, (String) newValue);
+            default:
+                break;
+        }
+
+        String node = Constants.sBooleanNodePreferenceMap.get(key);
+        if (!TextUtils.isEmpty(node) && FileUtils.isFileWritable(node)) {
+            Boolean value = (Boolean) newValue;
+            FileUtils.writeLine(node, value ? "1" : "0");
+            return true;
+        }
+        node = Constants.sStringNodePreferenceMap.get(key);
+        if (!TextUtils.isEmpty(node) && FileUtils.isFileWritable(node)) {
+            FileUtils.writeLine(node, (String) newValue);
+            return true;
+        }
+
         return false;
     }
 
     @Override
-<<<<<<< HEAD
     public void addPreferencesFromResource(int preferencesResId) {
         super.addPreferencesFromResource(preferencesResId);
         // Initialize node preferences
@@ -427,8 +407,6 @@ public class DeviceSettings extends PreferenceFragment
     }
 
     @Override
-=======
->>>>>>> parent of 5c570cd... sm8350-common: DeviceSettings: Import Awesome Alert Slider
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
         // Respond to the action bar's Up/Home button
